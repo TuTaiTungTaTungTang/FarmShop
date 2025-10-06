0f7695b12d7b4950900d45f1f4149b37f5d56877
TuTaiTungTaTungTang <taib2105559@student.ctu.edu.vn>
2025-10-07 00:22:38 +0700

    fix footer

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -17,9 +17,9 @@ const Footer = () => {
         <div className="bg-[#000] text-white">
             <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
                 <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-                    <span className="text-[#56d879]">ĐĂNG KÝ</span> TRANG CHÚNG TÔI ĐỂ ĐƯỢC NHẬN NHIỀU TIN TỨC{" "}
+                     <span className="text-[#56d879]">Subscribe</span> us for get news{" "}
                     <br />
-                    SỰ KIỆN VÀ ƯU ĐÃI
+                    events and offers
                 </h1>
                 <div>
                     <input
